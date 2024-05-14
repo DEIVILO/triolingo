e4ce98df22a0ce26b330266c55c3010c176e2db5
DEIVILO <ernestskrafts14@gmail.com>
2024-05-14 20:58:35 +0300

    done for the day, continue at user-progress.tsx

---
+++ components/user-progress.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    activeCourse: any;            //TODO: REPLACE WITH DATABASE COURSE
+    hearts: number;
+    points: number; 
+    hasActiveSubscription: boolean;
+}
+
+export const UserProgress = ({}: Props) => {
+    return (
+        <div>
+            User Progress
+        </div>
+    );
+}
